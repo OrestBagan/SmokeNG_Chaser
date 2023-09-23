c44efc1f03fff4919f9fc36e9e12434ef5deec81
OrestBagan <orestbagan2233@gmail.com>
2023-09-23 16:19:47 +0300

    jj

--- main.py
+++ main.py
@@ -4,17 +4,31 @@ from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
 
+tovar_list = ['✅Вишня 50 MG (10мл)', '✅Полуниця банан 50 MG (10мл)', '✅Балі тріпл шот 50 MG (10мл)', '✅Полнуниця 50 MG (10мл)', '✅Кола 50 MG (10мл)', '✅Ягоди ICE 50 MG (10мл)', '❌Ожиновий джем', "❌М'ята", '❌Персик ICE', '❌Блакитна малина-лимонад']
+tovar = ''
+for i in tovar_list:
+    tovar += i + '\n'
+text = '🔥 В наявності (Chaser)🔥\n\nChaser(salt nic.)\nЦіна 🔥: 1️⃣1️⃣0️⃣грн\n\n'
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     sti = open('img/sticker.webp', 'rb')
     bot.send_sticker(message.chat.id, sti)
 
-    murkup = types.InlineKeyboardMarkup(row_width=1)
-    button1 = types.InlineKeyboardButton('Ні якую', callback_data='no')
-    button2 = types.InlineKeyboardButton('Так звичайно', callback_data='yes')
+    murkup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.InlineKeyboardButton('Зробити замовлення')
 
-    murkup.add(button1, button2)
+    murkup.add(button)
 
-    bot.send_message(message.chat.id, 'Вітаємо у нашому магазині, бажаєте зробити замовлення?', reply_markup=murkup)
+    bot.send_message(message.chat.id, 'Вітаємо у телеграм боті <b><i>{1.first_name}</i></b>\nЦей бот створений для оформлення замовленя у нашому магазині'.format(message.from_user, bot.get_me()), parse_mode=('html'), reply_markup=murkup)
+
+@bot.message_handler(content_types=['text'])
+def func(message):
+    if message.chat.type == 'private':
+        if message.text == 'Зробити замовлення':
+            ph1 = open('img/photo_one.webp', 'rb')
+            bot.send_sticker(message.chat.id, ph1)
+            bot.send_message(message.chat.id, text + tovar)
 
 bot.polling(none_stop=True)
